c5aef9c24a4ef5edd43c762759cf81643a40354c
Yuna4cd <yuan4cd@foxmail.com>
2026-01-29 22:25:58 -0500

    Fix BrowserRouter basename for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import App from './App.jsx'
 
 ReactDom.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/evbuddy-homepage-jz">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
